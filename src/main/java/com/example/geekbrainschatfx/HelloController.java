0f5658fc8620996d456e35a62760a41100c75986
Dmitry Postupaylo <d.postupaylo@yandex.ru>
2022-06-07 23:12:03 +0300

    Form with input area  - send by click  - send by enter

--- src/main/java/com/example/geekbrainschatfx/HelloController.java
+++ src/main/java/com/example/geekbrainschatfx/HelloController.java
@@ -5,13 +5,31 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class HelloController {
     @FXML
-    private TextArea mainTextArea;
+    private TextArea conversationArea;
+
+    @FXML
+    private TextField messageInput;
 
     @FXML
     public void btnOneClickAction(ActionEvent actionEvent) {
-        mainTextArea.appendText("1\n");
+        sendMessage();
+    }
+
+    @FXML
+    public void msgAreaClickAction(KeyEvent keyEvent){
+        if (keyEvent.getCode() == KeyCode.ENTER)
+            sendMessage();
+    }
+
+    private void sendMessage(){
+        String message = messageInput.getText();
+        messageInput.clear();
+        conversationArea.appendText(String.format("%s\n", message));
     }
 }
