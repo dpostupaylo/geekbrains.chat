4756f487109bfd318b7262ac506e8acefeb3c6bd
Дмитрий <d.postupaylo@yandex.ru>
2022-09-18 23:27:15 +0200

    Feature implemented:  - mini version of JUnit framework

--- src/main/java/homework7/reflection/api/Main.java
+++ src/main/java/homework7/reflection/api/Main.java
@@ -10,27 +10,5 @@ public class Main {
     public static void main(String[] args) {
         TestLauncher launcher = new TestLauncher();
         launcher.start(TestClass.class);
-/*
-        Annotation[] annotations = clazz.getAnnotationsByType(Test.class);
-        System.out.println(annotations.length);
-
-        boolean isTypeAnnotationPresent =
-                clazz.getPackage().isAnnotationPresent(Test.class);
-        System.out.println(isTypeAnnotationPresent);
-
-// Target: Type
-        boolean isTypeAnnotationPresent1 =
-                clazz.isAnnotationPresent(Test.class);
-        System.out.println(isTypeAnnotationPresent1);
-
-// Target: Method
-        try{
-        Method method = clazz.getMethod("tearDown");
-        boolean isMethodAnnotationPresent =
-                method.isAnnotationPresent(AfterSuite.class);
-            System.out.println(isMethodAnnotationPresent);
-        }catch (NoSuchMethodException ex){
-            System.out.println(ex.getMessage());
-        }*/
     }
 }
